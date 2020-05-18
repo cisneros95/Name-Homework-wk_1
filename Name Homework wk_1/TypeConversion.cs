aca36bb710321995ee16453f7942c845837539ee
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-05-17 21:44:30 -0700

    Add project files.

---
+++ Name Homework wk_1/TypeConversion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Name_Homework_wk_1
+{
+    class TypeConversion
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+            
+        }
+    }
+}
